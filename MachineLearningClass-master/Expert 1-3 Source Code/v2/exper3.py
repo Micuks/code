39e123e8ce4faca0e3c3d5356f879cd98df16f4e
Micuks <sdkjwql@outlook.com>
2020-11-14 11:02:15 +0800

    11.14

--- MachineLearningClass-master/Expert 1-3 Source Code/v2/exper3.py
+++ MachineLearningClass-master/Expert 1-3 Source Code/v2/exper3.py
@@ -19,17 +19,20 @@ from tensorflow.examples.tutorials.mnist import input_data
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# disable eager execution
+tf.compat.v1.disable_eager_execution()
+
 # 载入数据
 mnist = input_data.read_data_sets("/tmp/tensorflow/mnist/input_data", one_hot=True)
 
 # 构建单层神经网络
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.compat.v1.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.matmul(x, W) + b
 
 # 定义损失函数和优化器
-y_ = tf.placeholder(tf.float32, [None, 10])
+y_ = tf.compat.v1.placeholder(tf.float32, [None, 10])
 
 # The raw formulation of cross-entropy,
 #
@@ -40,12 +43,12 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 #
 # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
 # outputs of 'y', and then average across the batch.
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+cross_entropy = tf.reduce_mean(input_tensor=tf.nn.softmax_cross_entropy_with_logits(labels=tf.stop_gradient(y_), logits=y))
 # 注意learning_rate
-train_step = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cross_entropy)
+train_step = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cross_entropy)
 
-sess = tf.InteractiveSession()
-tf.global_variables_initializer().run()
+sess = tf.compat.v1.InteractiveSession()
+tf.compat.v1.global_variables_initializer().run()
 # 训练模型:range内迭代次数
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
@@ -54,8 +57,8 @@ for i in range(1000):
         print("cross_entropy error:",sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys}))
 
 # 测试训练好的模型
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+correct_prediction = tf.equal(tf.argmax(input=y, axis=1), tf.argmax(input=y_, axis=1))
+accuracy = tf.reduce_mean(input_tensor=tf.cast(correct_prediction, tf.float32))
 print("test accuracy: ",sess.run(accuracy, feed_dict={x: mnist.test.images,
                                   y_: mnist.test.labels}))
 
@@ -64,8 +67,8 @@ print("test accuracy: ",sess.run(accuracy, feed_dict={x: mnist.test.images,
 p = 0
 
 s = sess.run(y,feed_dict={x: mnist.test.images[p].reshape(1,784)})
-print("Prediction : ",sess.run(tf.argmax(s, 1)))
+print("Prediction : ",sess.run(tf.argmax(input=s, axis=1)))
 
 #显示图片
-plt.imshow(mnist.test.images[p].reshape(28,28), cmap=plt.cm.gray_r, interpolation='nearest')
+plt.imshow(mnist.test.images[p].reshape(28,28), cmap=plt.cm.grey_r, interpolation='nearest')
 plt.show()
---
+++ MachineLearningClass-master/Expert 1-3 Source Code/v2/test.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+print(tf.__version__)
+from tensorflow.examples.tutorials.mnist import input_data
