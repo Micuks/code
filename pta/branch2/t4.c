48ce31b0d778e2426114a3319a45789c39781168
Micuks <sdkjwql@outlook.com>
2020-11-19 09:01:58 +0800

    11.19.2 AC

--- pta/branch2/t4.c
+++ pta/branch2/t4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int GCD(int a,int b) {
-    printf("a%%b = %d,b%%a = %d",a%b,b%a);
-    if(a==1||b==1) return a==1?b:a;
+    if(a==0||b==0) return a+b;
+    // printf("a%%b = %d,b%%a = %d",a%b,b%a);
     return a>b?GCD(b,a%b):GCD(a,b%a);
 }
 int main()
@@ -9,8 +9,7 @@ int main()
     int        a , b ;
 
     scanf("%d%d", &a , &b );
-    printf("a = %d,b = %d\n",a,b);
-    printf( "%d\n" , GCD( a, b ) ) ;
+    printf( "%d\n" , GCD(a,b) ) ;
 
     return 0 ;    
 }
