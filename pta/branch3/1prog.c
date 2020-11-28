f053004725ab9806418c5492cf5b8c705c5df2aa
Micuks <sdkjwql@outlook.com>
2020-11-28 11:20:22 +0800

    11.28.2

--- pta/branch3/1prog.c
+++ pta/branch3/1prog.c
@@ -1,24 +1,18 @@
 #include<stdio.h>
 #include<string.h>
-int isParenthesesmatch(char a[]) {
-    char temp;
-    int count=0,ptr=0;
-    while((temp=a[ptr++])!='\n'&&temp!=' ') {
-        // printf("temp = %c\t",temp);
-        if(temp=='(') count++;
-        if(temp==')') count--;
-        // printf("count = %d\n",count);
-        if(count<0) return -1;
-    }
-    if(0==count) return 0;
-    else return -1;
-}
 int main() {
     char string[300];
     memset(string,' ',sizeof(string));
     scanf("%s",string);
-    if(isParenthesesmatch(string)==0) printf("parentheres match!\n");
+    int count=0,ptr=0,flag=0;
+    while(string[ptr]!='\n'&&string[ptr]!=' ') {
+        // for(;ptr<100;ptr++) {
+        if(string[ptr]=='(') count++;
+        if(string[ptr]==')') count--;
+        if(count<0) flag=1;
+        ptr++;
+    }
+    if(count==0&&!flag) printf("parentheses match!\n");
     else printf("parentheses do not match!\n");
     return 0;
-
 }
