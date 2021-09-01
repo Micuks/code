c475718dc2def49e9f26271d702c7da4558faa14
micuks <sdkjwql@gmail.com>
2021-09-01 15:48:37 +0800

    uva679 practice binary tree

--- aoapc/chapter6/Dropping_Balls_uva_679.cpp
+++ aoapc/chapter6/Dropping_Balls_uva_679.cpp
@@ -1,2 +1,26 @@
 #include <cstdio>
 #include <cstring>
+
+const int maxn = 20;
+int tree[1<<maxn];
+
+int main() {
+    int d,n;
+    while(scanf("%d%d", &d, &n) == 2) {
+        memset(tree, 0, sizeof(tree));
+        int ptr = 1;
+        for(int i = 0; i < n; i++) {
+            while(ptr < 1<<(d-1)) {
+                if(tree[ptr] == 0) {
+                    ptr = ptr<<1;
+                }
+                else {
+                    ptr = ptr<<1+1;
+                }
+                printf("level: %d, ptr: %d\n",i, ptr);
+            }
+        }
+        printf("%d\n",tree[ptr]);
+    }
+    return 0;
+}
