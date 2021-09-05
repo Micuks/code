b24b988ba64ba9c48f96cefd72ed11f8c8b8d79b
micuks <sdkjwql@gmail.com>
2021-09-05 20:43:46 +0800

    tree_uva548 output sort algorithm-Heapsort

--- aoapc/chapter6/tree_uva548.cpp
+++ aoapc/chapter6/tree_uva548.cpp
@@ -8,32 +8,30 @@
 using namespace std;
 
 const int maxn = 10010;
-int root;
-bool input(queue<int>& vec);
-int buildtree(queue<int>& inorder, queue<int>& postorder);
-void clearqueue(queue<int>& q);
-int tleft[maxn], tright[maxn];
+int root, cnt = 0;
+int tleft[maxn], tright[maxn], value[maxn], sum[maxn];
 
-int main() {
-    queue<int> inorder, postorder;
-    while(input(inorder)) {
-        memset(tleft, 0, sizeof(tleft));
-        memset(tright, 0, sizeof(tright));
-        input(postorder);
-        //int len = inorder.size();
-        //for(int i = 0; i < len; i++) {
-        //    cout << inorder[i] << ' ';
-        //}
-        //len = postorder.size();
-        //for(int i = 0; i < len; i++) {
-        //    cout << postorder[i] << ' ';
-        //}
-        root = buildtree(inorder, postorder);
+int addnode(void) {
+    int node = ++cnt;
+    tleft[node] = tright[node] = value[node] = 0;
+    return node;
+}
+
+void pq(queue<int> tin) {
+    while(!tin.empty()) {
+        cout << tin.front() << ' ';
+        tin.pop();
     }
-    return 0;
+    cout << endl;
+}
+
+void clearqueue(queue<int>& q) {
+    queue<int> empty;
+    swap(empty, q);
 }
 
 bool input(queue<int>& vec) {
+    clearqueue(vec);
     string str;
     int num;
     if(!getline(cin, str))
@@ -44,22 +42,95 @@ bool input(queue<int>& vec) {
     return true;
 }
 
-void clearqueue(queue<int>& q) {
-    queue<int> empty;
-    swap(empty, q);
-}
-int buildtree(queue<int>& inorder, queue<int>& postorder) {
-    root = postorder.back();
+int buildtree(int pvalue, queue<int> inorder, queue<int> postorder) {
+    int node = addnode();
+    value[node] = postorder.back();
+    sum[node] = pvalue + value[node];
+    //cout << "value[node] = " << value[node] << endl;
     queue<int> oldin = inorder, oldpo = postorder;
     queue<int> tmpin, tmppo;
-    while(oldin.front() != root) {
+    while(oldin.front() != value[node]) {
         tmpin.push(oldin.front());
         tmppo.push(oldpo.front());
         oldin.pop();
         oldpo.pop();
     }
+    //cout << "oldin: ";
+    //pq(oldin);
+    //cout << "oldpo: ";
+    //pq(oldpo);
+    //cout << "tmpin: ";
+    //pq(tmpin);
+    //cout << "tmppo: ";
+    //pq(tmppo);
     oldin.pop();
-    tleft[root] = buildtree(tmpin, tmppo);
+    if(!tmpin.empty() || !tmppo.empty()) {
+        //cout << "add left subtree" << endl;
+        tleft[node] = buildtree(value[node], tmpin, tmppo);
+    }
     clearqueue(tmpin);clearqueue(tmppo);
-    return root;
+    while(!oldin.empty()) {
+        tmpin.push(oldin.front());
+        tmppo.push(oldpo.front());
+        oldin.pop();
+        oldpo.pop();
+    }
+    //cout << "oldin: ";
+    //pq(oldin);
+    //cout << "oldpo: ";
+    //pq(oldpo);
+    //cout << "tmpin: ";
+    //pq(tmpin);
+    //cout << "tmppo: ";
+    //pq(tmppo);
+    if(!tmpin.empty() || !tmppo.empty()) {
+        //cout << "add right subtree" << endl;
+        tright[node] = buildtree(value[node], tmpin, tmppo);
+    }
+    return node;
 }
+
+void debugtree(void) {
+    queue<int> q;
+    vector<int> ans;
+    ans.clear();
+    q.push(root);
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        ans.push_back(value[node]);
+        if(tleft[node])
+            q.push(tleft[node]);
+        if(tright[node])
+            q.push(tright[node]);
+    }
+    int len = ans.size();
+    cout << "print tree" << endl;
+    for(int i = 0; i < len; i++) {
+        cout << ans[i] << ' ';
+    }
+    cout << endl;
+}
+
+void output(void) {
+    int ndmin = maxn;
+    for(int i = 1; i <= cnt; i++) {
+        
+    }
+}
+int main() {
+    queue<int> inorder, postorder;
+    while(input(inorder)) {
+        input(postorder);
+
+        cnt = 0;
+        //memset(tleft, 0, sizeof(tleft));
+        //memset(tright, 0, sizeof(tright));
+        //memset(value, 0, sizeof(value));
+        memset(sum, 0, sizeof(sum));
+        root = buildtree(0, inorder, postorder);
+        debugtree();
+    }
+    return 0;
+}
+
