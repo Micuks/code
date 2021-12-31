6506d967ba089f8e00fdeb01229a87399e980a71
micuks <sdkjwql@gmail.com>
2021-12-31 11:49:42 +0800

    Winter Holidaygit add -A

--- oj/dSCourse/dynamic_search/src/main.cpp
+++ oj/dSCourse/dynamic_search/src/main.cpp
@@ -12,18 +12,31 @@ int main() {
     for(int i = 0; i < n; i++) {
         scanf("%d", &tmp);
         array[i] = tmp;
-        InsertBST(T, tmp);
+        //InsertBST(T, tmp);
     }
+    printf("编号\t");
     for(int i = 0; i < n; i++) {
         printf("%d\t", i);
     }
     putchar('\n');
+    printf("数据\t");
     for(int i = 0; i < n; i++) {
         printf("%d\t", array[i]);
     }
     putchar('\n');
+    for(int i = 0; i < n; i++) {
+        InsertBST(T, array[i]);
+    }
+    printf("中序遍历BST树结果\n");
     InOrderVisit(T, visit);
     putchar('\n');
+
+    int key = 0;
+    scanf("%d", &key);
+    printf("key = %d\n", key);
+    printf("关键字不小于key的元素由大到小输出为\n");
+    T4TraverseBST(T, key);
+
     DestroyTree(T);
 
     return 0;
