87edcae802bbcc686fae108153ca23fa7970dbac
micuks <sdkjwql@outlook.com>
2022-03-05 12:00:34 +0800

    section 456

--- code.c
+++ code.c
@@ -1,15 +1,5 @@
 #include<stdio.h>
-#include <stdarg.h>
-void print(const char * format, ...) {
-    va_list(args);
-    va_start(args, format);
-    printf(format, args);
-    va_end(args);
-}
 int main() {
-    int a = 10;
-    char c = 'c';
-    printf("%d a %c\n", a, c);
-    print("%d a %c\n", a, c);
-    return 0;
+	printf("test");
+	return 0;
 }
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,8 @@
-#include <stdio.h>
-#include <math.h>
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    int max(int a, int b);
-    int a, b, c, d;
-    scanf("%d%d%d", &a, &b, &c);
-    if(a >= b && a >= c)
-        d = max(a, b);
-    else {
-        if(b >= a && b >= c)
-            d = max(a, b);
-        else {
-        d = max(a, b);
-        }
-    }
-    printf("%d", d);
+    cout << "Hello!\n";
+    cout << "Welcome to C++!\n";
 }
