b4621c6f62e3c10eaf7f5473fbe9c4e75257024d
micuks <sdkjwql@gmail.com>
2022-05-29 01:21:59 +0800

    [feature] delete single generator

--- automata/cfg2cnf2gnf.py
+++ automata/cfg2cnf2gnf.py
@@ -45,12 +45,7 @@ class CFG:
                         i = jtem.find(ktem, i+1)
 
     def add_S1(self):
-        set_u = set()
-        for item in self.grammar:
-            for jtem in self.grammar[item]:
-                for ktem in jtem:
-                    if jtem.isupper():
-                        set_u.add(ktem)
+        set_u = self.get_set_N()
         for item in alphabet:
             if item not in set_u:
                 self.grammar.update({item: [epsilon]})
@@ -58,6 +53,15 @@ class CFG:
                 self.start = item
                 return
 
+    def get_set_N(self):
+        set_n = set()
+        for item in self.grammar:
+            for jtem in self.grammar[item]:
+                for ktem in jtem:
+                    if ktem in alphabet:
+                        set_n.add(ktem)
+        return set_n
+                    
     def in_set(self, jtem, set):
         flag = True
         for c in jtem:
@@ -66,10 +70,28 @@ class CFG:
                 break
         return flag
 
+    def delete_single_generator(self):
+        for item in self.grammar:
+            set0 = set()
+            set1 = set(item)
+            while set0 != set1:
+                set0 = set1.copy()
+                for jtem in set0:
+                    for ktem in self.grammar[jtem]:
+                        if ktem not in set0 and len(ktem) == 1 and ktem in alphabet:
+                            set1.add(ktem)
+            print(set1)
+            for jtem in set1:
+                self.grammar[item].extend(self.grammar[jtem])
+                self.grammar[item] = list(set(self.grammar[item]))
+                if jtem in self.grammar[item]:
+                    self.grammar[item].remove(jtem)
+
+
 def main():
     n = int(input("Input the number of grammars.\n"))
     for i in range(n):
-        str = input()
+        str = input("Input grammar No.{}\n".format(i))
         sstr = str.split('->')
         key = sstr[0].strip()
         vals = sstr[1].split('|')
@@ -78,6 +100,7 @@ def main():
     print(grammars)
     g = CFG(grammars, 'S')
     g.delete_epsilon()
+    g.delete_single_generator()
     print(grammars)
 
 if __name__ == "__main__":
