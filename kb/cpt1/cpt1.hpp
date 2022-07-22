b930f924c2df6e399010622a831b7c6cba72b4f4
micuks <sdkjwql@gmail.com>
2022-07-22 12:45:25 +0800

    poj-1321 ac

--- kb/cpt1/cpt1.hpp
+++ kb/cpt1/cpt1.hpp
@@ -17,7 +17,6 @@ const int maxn = 10;
 
 int g_flag[maxn][maxn];
 int n, k;
-int cnt = 0;
 
 void printMatrix(int mtx[][maxn]) {
     for (int i = 0; i < n; i++) {
@@ -43,33 +42,36 @@ int process(int ns, int iter_depth, int mtx[][maxn]) {
         return 1;
     }
     if (iter_depth + n - ns < k) {
+        // printf("0\n");
         return 0;
     }
+    // printf("ns[%d]\n", ns);
     int i = ns;
     for (int j = 0; j < n; j++) {
         if (flag[i][j] == 1) {
+            // printf("%d, %d\n", i, j);
             memset(flag[i], 0, maxn * sizeof(int));
-            for (int k = 0; k < maxn; k++) {
-                flag[i][k] = 0;
+            for (int l = 0; l < maxn; l++) {
+                flag[l][j] = 0;
             }
-            sum += process(ns + 1, iter_depth + 1, flag);
+            sum += process(i + 1, iter_depth + 1, flag);
             memcpy(flag[i], mtx[i], maxn * sizeof(int));
-            for (int k = 0; k < maxn; k++) {
-                flag[i][k] = mtx[i][k];
+            for (int l = 0; l < maxn; l++) {
+                flag[l][j] = mtx[l][j];
             }
-        } else {
-            sum += process(ns + 1, iter_depth, flag);
         }
     }
+    sum += process(i + 1, iter_depth, flag);
     return sum;
 }
 
 int main(int argc, char **argv) {
     while (scanf("%d%d", &n, &k) == 2 && n != -1 && k != -1) {
+        memset(g_flag, 0, sizeof(g_flag));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                char tmp;
-                if ((tmp = getchar()) == '\n') {
+                char tmp = getchar();
+                while (tmp == '\n') {
                     tmp = getchar();
                 }
                 if (tmp == '#') {
