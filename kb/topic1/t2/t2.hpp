300c00e55ee127cc14bd906b387c94061bd7a9ce
micuks <sdkjwql@gmail.com>
2022-07-22 23:54:14 +0800

    my solution ideas

--- kb/topic1/t2/t2.hpp
+++ kb/topic1/t2/t2.hpp
@@ -1,20 +1,51 @@
 #ifndef _t2_20220722_
 #define _t2_20220722_
-#include <iostream>
+#include <algorithm>
+#include <assert.h>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cmath>
-#include <string>
-#include <algorithm>
-#include <vector>
+#include <iostream>
 #include <map>
-#include <set>
 #include <queue>
-#include <assert.h>
+#include <set>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(int argc, char** argv) {
+const int MAXN = 40;
+char maze[MAXN][MAXN][MAXN];
+class Point {
+  public:
+    int x;
+    int y;
+    int z;
+    int distance;
+    Point() : x(-1), y(-1), z(-1), distance(-1) {}
+    Point(int _x, int _y, int _z) : x(_x), y(_y), z(_z) {}
+};
+
+Point data[MAXN][MAXN][MAXN];
+
+void dijkstra(Point begin, Point end) {}
+
+int main(int argc, char **argv) {
+    int l, r, c;
+    while (scanf("%d%d%d", &l, &r, &c) == 3 && (l != 0 && r != 0 && c != 0)) {
+        memset(maze, '#', MAXN * MAXN * MAXN * sizeof(int));
+        memset(distance, -1, MAXN * MAXN * MAXN * sizeof(int));
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                char tmp = getchar();
+                while (tmp == '\n') {
+                    tmp = getchar();
+                }
+                maze[i][j] = tmp;
+            }
+        }
+        dijkstra();
+    }
     return 0;
 };
 
