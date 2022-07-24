4ec127b00175f22b3a5e0530d4f15e3704e39791
micuks <sdkjwql@gmail.com>
2022-07-24 22:19:41 +0800

    poj-3126 bfs ac

--- kb/topic1/t6/t6.hpp
+++ kb/topic1/t6/t6.hpp
@@ -14,10 +14,11 @@
 #include <vector>
 using namespace std;
 const int MAXN = 100000;
+int visited[MAXN];
 int best_cost = MAXN;
 
 bool isPrime(int num) {
-    int n = (int)sqrt(num) + 1;
+    int n = static_cast<int>(sqrt(num)) + 1;
     for (int i = 2; i < n; i++) {
         if (num % i == 0) {
             return false;
@@ -26,30 +27,54 @@ bool isPrime(int num) {
     return true;
 }
 
-int dfs(int begin, int end, int layer = 0, int cost = 0) {
-    if (begin == end) {
-        if (cost < best_cost)
-            best_cost = cost;
-        return 0;
+class State {
+  public:
+    int number;
+    int cost;
+    State() : number(-1), cost(0) {}
+    State(const int &_number, const int &_cost)
+        : number(_number), cost(_cost) {}
+    State(const State &_state) : number(_state.number), cost(_state.cost) {}
+};
+
+int qpow(int b, int p) {
+    int ans = 1;
+    while(p) {
+        if(p&0x1)
+            ans *= b;
+        b *= b;
+        p>>=1;
     }
-    if (layer == 4) {
-        return 0;
-    }
-    for (int i = (layer == 3) ? 1 : 0; i < 10; i++) {
-        // printf("%d, %d, %d\n", begin / (int)pow(10, layer + 1) * (int)pow(10,
-        // layer + 1),
-        //        begin % (int)pow(10, layer), i * (int)pow(10, layer));
-        int tmp = (begin / (int)pow(10, layer + 1) * (int)pow(10, layer + 1)) +
-                (begin % (int)pow(10, layer)) + (int)pow(10, layer) * i;
-        if(tmp == begin) {
-            continue;
+    return ans;
+}
+
+int ireplace(int num, int offset, int k) {
+    offset = qpow(10, offset);
+    return num / (offset * 10) * (offset * 10) + num % offset + k * offset;
+}
+
+int bfs(int begin, int end) {
+    queue<State> q;
+    memset(visited, 0, sizeof(visited));
+    visited[begin] = 1;
+    q.push(State(begin, 0));
+    while (!q.empty()) {
+        State s = q.front();
+        q.pop();
+        if (s.number == end) {
+            return s.cost;
         }
-        if (isPrime(tmp)) {
-            printf("tmp[%d],layer[%d],cost[%d]\n", tmp, layer, cost);
-            dfs(tmp, end, layer + 1, cost + 1);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 10; j++) {
+                int a = ireplace(s.number, i, j); //第i 位替换为j
+                if (a >= 1000 && a != s.number && !visited[a] && isPrime(a)) {
+                    visited[a] = 1;
+                    q.push(State(a, s.cost + 1));
+                }
+            }
         }
     }
-    return 0;
+    return MAXN;
 }
 
 int main(int argc, char **argv) {
@@ -59,7 +84,7 @@ int main(int argc, char **argv) {
         best_cost = MAXN;
         int begin, end;
         scanf("%d%d", &begin, &end);
-        dfs(begin, end);
+        best_cost = bfs(begin, end);
         if (best_cost < MAXN) {
             cout << best_cost << endl;
         } else {
