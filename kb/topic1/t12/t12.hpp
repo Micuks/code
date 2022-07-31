06e93f14f82edb4a13082cac545d9f97fb5ed9f6
micuks <sdkjwql@gmail.com>
2022-07-31 17:32:45 +0800

    hdu-1241 ac dfs

--- kb/topic1/t12/t12.hpp
+++ kb/topic1/t12/t12.hpp
@@ -1,3 +1,4 @@
+// #define db
 #ifndef _t12_20220731_
 #define _t12_20220731_
 #include <algorithm>
@@ -12,31 +13,33 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <stack>
 using namespace std;
 const int MAXN = 1000;
 char maze[MAXN][MAXN];
 int deposits[MAXN][MAXN];
-queue<pair<int, int>> q;
+stack<pair<int, int>> q;
 int cnt = 0;
 typedef pair<int, int> tp;
-const int dirs[][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const int dirs[][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1},{1,1},{1,-1},{-1,1},{-1,-1}};
 template <typename t> void printMaze(int m, int n, t maze[][MAXN]) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            cout << maze[i][j];
+            cout<<maze[i][j];
         }
         cout << endl;
     }
 }
 bool isValid(int x, int y, int m, int n) {
-    return (x >= 0 && x < m && y >= 0 && y < n);
+    return (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == '@');
 }
-void bfs(int m, int n) {
+void dfs(int m, int n) {
     while (!q.empty()) {
-        tp p = q.front();
+        // tp p = q.front();
+        tp p = q.top();
         q.pop();
         if (deposits[p.first][p.second] == -1) {
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 8; i++) {
                 int nx = p.first + dirs[i][0];
                 int ny = p.second + dirs[i][1];
                 if (isValid(nx, ny, m, n) && deposits[nx][ny] != -1) {
@@ -47,7 +50,7 @@ void bfs(int m, int n) {
         if (deposits[p.first][p.second] == -1) {
             deposits[p.first][p.second] = cnt++;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 8; i++) {
             int nx = p.first + dirs[i][0];
             int ny = p.second + dirs[i][1];
             if (isValid(nx, ny, m, n) && deposits[nx][ny] == -1) {
@@ -55,6 +58,11 @@ void bfs(int m, int n) {
                 q.push(make_pair(nx, ny));
             }
         }
+#ifdef db
+        cout << "---print\n";
+        printMaze(m, n, deposits);
+        cout << "deposits---\n";
+#endif
     }
 }
 int main(int argc, char **argv) {
@@ -68,7 +76,7 @@ int main(int argc, char **argv) {
             // cin.getline(maze[i], MAXN, '\n');
             // cout<<maze[i]<<endl;
             for (int j = 0; j < n; j++) {
-                cin>>maze[i][j];
+                cin >> maze[i][j];
                 if (maze[i][j] == '@') {
                     q.push(make_pair(i, j));
                 }
@@ -79,7 +87,7 @@ int main(int argc, char **argv) {
         printMaze(m, n, maze);
         cout << "maze---" << endl;
 #endif
-        bfs(m, n);
+        dfs(m, n);
         cout << cnt << endl;
     }
     return 0;
