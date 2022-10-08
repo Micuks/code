da77b6535a6ef7a2372de79f41b37329e2dfc00c
Micuks <sdkjwql@gmail.com>
2022-10-08 11:48:30 +0800

    mergesort docs section 1 & 2

--- algorithm/mergesort_and_quicksort/mergesort/mergesort.cpp
+++ algorithm/mergesort_and_quicksort/mergesort/mergesort.cpp
@@ -16,10 +16,11 @@ class MergeSort {
         a = new int[n + 1];
         b = new int[n + 1];
         memcpy(a, _a, _n * sizeof(int));
-        printA();
-        printB();
     }
-    ~MergeSort() { delete[] a; }
+    ~MergeSort() {
+        delete[] a;
+        delete[] b;
+    }
     void mergeSort();
     void printA() {
         std::cout << "Array A:\n";
@@ -35,6 +36,12 @@ class MergeSort {
         }
         std::cout << std::endl;
     }
+    void print() {
+        for (int i = 0; i < n; i++) {
+            std::cout << a[i] << " ";
+        }
+        std::cout << std::endl;
+    }
 
   private:
     void mergePass(int x[], int y[], int s, int n);
@@ -47,36 +54,26 @@ class MergeSort {
 };
 
 /**
- * Iterative mergesort function to sort a[0, ..., n-1]
+ * Iterative mergesort function to sort a[0, ..., n-1], 
+ * Merge subarrays in bottam up manner. First merge subarrays of
+ * size 1 to create sorted subarrays of size 2, then merge subarrays
+ * of size 2 to create sorted subarrays of size 4, and vice versa.
  */
 void MergeSort::mergeSort() {
     // merge step size
     int s = 1;
-    cout<<endl;
     while (s < n) {
         // treat b as assistant array
         mergePass(a, b, s, n);
-        // exponential increase step size
-        cout << "s["<<s<<"]"<<endl;
-        printA();
-        printB();
-        cout<<endl;
         s += s;
         // treat a as assistant array
         mergePass(b, a, s, n);
-        // exponential increase step size
-        cout << "s["<<s<<"]"<<endl;
-        printA();
-        printB();
-        cout<<endl;
         s += s;
     }
 }
 
 /**
- * Merge subarrays in bottam up manner. First merge subarrays of
- * size 1 to create sorted subarrays of size 2, then merge subarrays
- * of size 2 to create sorted subarrays of size 4, and vice versa.
+ * A pass of merge with step size of s.
  */
 void MergeSort::mergePass(int *x, int *y, int s, int n) {
     int i = 0;
@@ -104,12 +101,16 @@ void MergeSort::merge(int *c, int *d, int l, int m, int r) {
     int k = i;
     // Merge the two halves of array c to array d
     while ((i <= m) && (j <= r)) {
+        // append smaller element of c[i] and c[j] to d[k]
+        // and increase i and k or j and k
         if (c[i] <= c[j]) {
             d[k++] = c[i++];
         } else {
             d[k++] = c[j++];
         }
     }
+    // if left half of array c reaches the end, append rest elements
+    // of right half of array c to d
     if (i > m) {
         // Copy the remaining elements in the left half of c to d
         while (j <= r) {
@@ -124,6 +125,7 @@ void MergeSort::merge(int *c, int *d, int l, int m, int r) {
 }
 
 int main(int argc, char **argv) {
+    ios_base::sync_with_stdio(false);
     std::fstream fs;
     // Will attempt to open given file if received more then one
     // parameters.
@@ -133,12 +135,12 @@ int main(int argc, char **argv) {
         } catch (std::system_error &e) {
             std::cerr << e.code().message() << std::endl;
         }
-        std::cout << argv[1] << std::endl; // debug
+        // std::cout << argv[1] << std::endl; // debug
     } else {
         // Open default input file
         try {
             char filename[] = "samples/mergesort.in";
-            std::cout << filename << std::endl; // debug
+            // std::cout << filename << std::endl; // debug
             fs.open(filename, std::ios_base::in);
         } catch (std::system_error &e) {
             std::cerr << e.code().message() << std::endl;
@@ -152,11 +154,11 @@ int main(int argc, char **argv) {
         arr.push_back(num);
     }
     fs.close();
-    std::cout << std::endl;
-    MergeSort mSort(&arr[0], arr.size());
-    mSort.mergeSort();
-    mSort.printA();
-    mSort.printB();
-
+    void *p = new char[sizeof(MergeSort)];
+    MergeSort *pMergeSort = new (p) MergeSort(&arr[0], arr.size());
+    pMergeSort->mergeSort();
+    pMergeSort->print();
+    pMergeSort->~MergeSort();
+    delete[] (char *)p;
     return 0;
 }
