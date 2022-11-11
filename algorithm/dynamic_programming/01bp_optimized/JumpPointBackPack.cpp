ea038ed5fac89f86161c47a46dbcba633a8d3dc0
Micuks <sdkjwql@gmail.com>
2022-11-11 09:25:00 +0800

    [algorithm] Fixed collect items in backpack bug.

--- algorithm/dynamic_programming/01bp_optimized/JumpPointBackPack.cpp
+++ algorithm/dynamic_programming/01bp_optimized/JumpPointBackPack.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <assert.h>
 #include <chrono>
-#include <cstring>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -11,13 +10,23 @@
 #include <utility>
 #include <vector>
 
-class BackPack01 {
+class Pack {
   public:
-    BackPack01(std::istream &is, bool verbose = false) {
+    Pack(int weight = 0, int val = 0) : weight(weight), val(val) {}
+    int weight;
+    int val;
+    friend bool operator<(const Pack &ba, const Pack &bb) {
+        return (ba.weight != bb.weight) ? ba.weight > bb.weight
+                                        : ba.val < bb.val;
+    }
+};
+
+class BackPackJumpPoint {
+  public:
+    BackPackJumpPoint(std::istream &is, bool verbose = false) {
         // Initialize array that dp uses.
         for (int i = 0; i < MAXN; i++) {
             dp[i] = new int[MAXN];
-            std::memset(dp[i], 0, MAXN * sizeof(int));
         }
 
         // Read data from is.
@@ -30,7 +39,7 @@ class BackPack01 {
         trackBackPackStatus = verbose;
     }
 
-    BackPack01(std::fstream &fs, bool verbose = false) {
+    BackPackJumpPoint(std::fstream &fs, bool verbose = false) {
         // Initialize array that dp uses.
         for (int i = 0; i < MAXN; i++) {
             dp[i] = new int[MAXN];
@@ -45,7 +54,7 @@ class BackPack01 {
         trackBackPackStatus = verbose;
     }
 
-    ~BackPack01() {
+    ~BackPackJumpPoint() {
         delete[] w;
         delete[] val;
 
@@ -65,89 +74,114 @@ class BackPack01 {
         return ss.str();
     }
 
-    void backPackDP(); // DP to calculate best maxVal.
-    void backTrace();  // Back track to collect items in the backpack.
-
+    void JumpPointBackPackDP(); // BackPack DP using jump point optimization.
     int getMaxVal() const { return maxVal; }
 
     std::string itemsInMaxValBackPackToString() {
         std::stringstream ss;
-        ss << std::setw(6) << "Weight"
+        ss << std::setw(4) << "Item"
+           << " | " << std::setw(6) << "Weight"
            << " | " << std::setw(5) << "Value"
            << " |" << std::endl;
+
         for (auto &a : itemsInMaxValBackPack) {
-            ss << std::setw(6) << w[a] << " | " << std::setw(5) << val[a]
-               << " |" << std::endl;
+            ss << std::setw(4) << a << " | " << std::setw(6) << w[a] << " | "
+               << std::setw(5) << val[a] << " |" << std::endl;
         }
         return ss.str();
     }
 
-    friend std::ostream &operator<<(std::ostream &os, BackPack01 &bp);
+    friend std::ostream &operator<<(std::ostream &os, BackPackJumpPoint &bp);
 
   private:
     bool trackBackPackStatus = false; // Track items in backpack at every state.
     int maxVal;                       // Max total value of items in backpack.
     int volume;                       // Volume of backpack.
     int n;                            // Number of items.
-    std::vector<int> itemsInMaxValBackPack;
 
     static const int MAXN = 2600;
-
     int *w = new int[MAXN];   // Weight of items
     int *val = new int[MAXN]; // Value of items
+    Pack pack[MAXN];
 
     int **dp = new int *[MAXN]; // dp[MAXN][MAXN], dp[item][volume].
+
+    class BackPackState {
+      public:
+        BackPackState(int newItemID, int previousItemID, int previousVolume)
+            : newItem(newItemID),
+              previousState(std::make_pair(previousItemID, previousVolume)) {}
+
+        int newItem;
+        std::pair<int, int> previousState; // Link-list-like store method
+    };
+
+    class PairHasher {
+      public:
+        size_t operator()(const std::pair<int, int> &obj) const {
+            return (obj.first) * 10000 + (obj.second);
+            // return (obj.first);
+        }
+    };
+
+    std::unordered_map<std::pair<int, int>, BackPackState, PairHasher>
+        backPackStateMap; // backPackState(itemID, remainedVolume) =
+    // (itemJustInsertedID,
+    // previousBackPackState(itemID, remainedVolume)
+    std::vector<int> itemsInMaxValBackPack;
 };
 
-void BackPack01::backTrace() {
-    int i = n, j = volume;
-    std::queue<int> q;
-    while (i > 0) {
-        int curr = dp[i][j];
-        if (dp[i - 1][j] == curr) {
-            i--;
-        } else {
-            q.push(--i); // TODO: To verify.
-            j -= w[i];
+void BackPackJumpPoint::JumpPointBackPackDP() {
+    std::priority_queue<Pack> pq;
+    std::queue<Pack> turn;
+    Pack initPack;
+    pq.push(initPack);
+    for (int i = 0; i < n; i++) {
+        std::priority_queue<Pack> newPq;
+        for (int j = 0; j < pq.size(); j++) {
+            Pack tmp;
+            tmp.weight = pq.top().weight + w[i];
+            tmp.val = pq.top().val + val[i];
+            newPq.push(pq.top());
+            newPq.push(tmp);
+            pq.pop();
         }
-    }
-    while (!q.empty()) {
-        itemsInMaxValBackPack.push_back(q.front());
-        q.pop();
-    }
-}
-
-std::ostream &operator<<(std::ostream &os, BackPack01 &bp) {
-    os << bp.getMaxVal();
-    if (bp.trackBackPackStatus) {
-        os << std::endl;
-        os << bp.itemsInMaxValBackPackToString();
-    }
-    return os;
-}
-
-void BackPack01::backPackDP() {
-    maxVal = -1;
-
-    // Initialize
-    for (int j = 0; j <= volume; j++) {
-        dp[n - 1][j] = (j >= w[n - 1]) ? val[n - 1] : 0;
-    }
-
-    int i = n - 2, j = volume;
-    for (; i >= 0; i--) {
-        j = volume;
-        for (; j >= 0; j--) {
-
-            // Compare total value of items in the backpack between put
-            // item[i] in and not put it in.
-            dp[i][j] = (j >= w[i]) ? std::max(dp[i + 1][j],
-                                              dp[i + 1][j - w[i]] + val[i])
-                                   : dp[i + 1][j];
+        while (!newPq.empty()) {
+            pq.push(newPq.top());
+            newPq.pop();
         }
     }
 
-    maxVal = dp[0][volume];
+    int preWeight = 0;
+    int preVal = 0;
+    while (!pq.empty()) {
+        if ((preWeight != pq.top().weight || preWeight == 0) &&
+            pq.top().weight <= volume) {
+            if (preVal >= pq.top().val) {
+                pq.pop();
+                continue;
+            }
+            turn.push(pq.top());
+            preWeight = pq.top().weight;
+            preVal = pq.top().val;
+        }
+        pq.pop();
+    }
+
+    std::cout << "Jump points:" << std::endl;
+
+    std::stringstream ss;
+    ss << std::setw(6) << "Weight"
+       << " | " << std::setw(5) << "Value"
+       << " |" << std::endl;
+
+    while (!turn.empty()) {
+        auto &a = turn.front();
+        ss << std::setw(6) << a.weight << " | " << std::setw(5) << a.val << " |"
+           << std::endl;
+        turn.pop();
+    }
+    std::cout << ss.str();
 }
 
 int main(int argc, char **argv) {
@@ -174,16 +208,15 @@ int main(int argc, char **argv) {
 
     assert(fs.is_open() == true);
 
-    void *p = new char[sizeof(BackPack01)];
-    bool verbose = true;
-    BackPack01 *backPack01 = new (p) BackPack01(fs, verbose);
+    void *pJ = new char[sizeof(BackPackJumpPoint)];
+    BackPackJumpPoint *backPackJumpPoint = new (pJ) BackPackJumpPoint(fs, true);
     fs.close();
 
-    std::cout << backPack01->constructorDebugString();
+    std::cout << backPackJumpPoint->constructorDebugString();
 
     auto begin = std::chrono::high_resolution_clock::now();
 
-    backPack01->backPackDP();
+    backPackJumpPoint->JumpPointBackPackDP();
 
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed =
@@ -198,12 +231,6 @@ int main(int argc, char **argv) {
     // fs << *backPack01;
     // fs.close();
 
-    if (verbose) {
-        backPack01->backTrace();
-    }
-    std::cout << *backPack01 << std::endl;
-
-    backPack01->~BackPack01();
-    delete[] (char *)p;
+    delete[] (char *)pJ;
     return 0;
 }
