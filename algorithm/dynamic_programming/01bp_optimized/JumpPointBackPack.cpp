91b33e7a750acac248673554e6b306ec34cd132f
Micuks <sdkjwql@gmail.com>
2022-11-11 21:47:47 +0800

    [BackPack] Finished docs.

--- algorithm/dynamic_programming/01bp_optimized/JumpPointBackPack.cpp
+++ algorithm/dynamic_programming/01bp_optimized/JumpPointBackPack.cpp
@@ -213,7 +213,7 @@ int main(int argc, char **argv) {
     } else {
         // Open default input file
         try {
-            char filename[] = "../samples/bag.in";
+            char filename[] = "samples/bag.in";
             fs.open(filename, std::ios_base::in);
         } catch (std::system_error &e) {
             std::cerr << e.code().message() << std::endl;
