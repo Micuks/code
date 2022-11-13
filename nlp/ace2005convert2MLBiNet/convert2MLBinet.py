6437233bf5f07c12b93e5b4c346d164f46b3ad0d
Micuks <sdkjwql@gmail.com>
2022-11-13 18:39:40 +0800

    [nlp] finished MLBiNet format dataset convertor

--- nlp/ace2005convert2MLBiNet/convert2MLBinet.py
+++ nlp/ace2005convert2MLBiNet/convert2MLBinet.py
@@ -1,21 +1,111 @@
 import argparse
+import re
 import json
 
 
-def preprocess(process_type, filename, debug=False):
+def replace_bracket(word: str):
+    if word == "(":
+        return "-LRB-"
+    elif word == ")":
+        return "-RRB-"
+    elif word == "[":
+        return "-LSB-"
+    elif word == "]":
+        return "-RSB-"
+    elif word == "{":
+       return "-LCB-"
+    elif word=="}":
+        return "-RCB-"
+    
+    return word
+
+
+def flatten(lists=[]):
     result = []
+    for l in lists:
+        if len(l) == 1:
+            result = lists
+            break
+        else:
+            result = l
+            break
+
+    return result
+
+
+def get_attributes(
+        word_offset: int, ners=[], relations=[], events=[], debug=False):
+    # Initialize er, relation and event of word
+    word_ner = "O"
+    word_relation = "O"
+    word_event = "O"
+
+    for lst in ners:
+        begin_ner, end_ner, identifier_ner = lst
+
+        if word_offset == begin_ner:
+            word_ner = "B-1_"+identifier_ner
+            break
+        elif begin_ner < word_offset <= end_ner:
+            word_ner = "I-1_"+identifier_ner
+            break
+
+    for lst in relations:
+        offset_pairs = lst[:-1]
+        identifier_relation = lst[-1]
+
+        # If relation already found, ignore following relations. Even though
+        # they may contaion the word.
+        if (word_relation.find("B") == 0) or (word_relation.find("I") == 0):
+            break
+
+        # Find word in offset pairs.
+        it_pairs = iter(offset_pairs)
+        for begin_relation in it_pairs:
+            end_relation = next(it_pairs)
+
+            if word_offset == begin_relation:
+                word_relation = "B-2_"+identifier_relation
+                break
+            elif begin_relation < word_offset <= end_relation:
+                word_relation = "I-2_"+identifier_relation
+                break
+
+    for lst in events:
+        # Event in [offset, "str"] format.
+        if(isinstance(lst[1], str)):
+            offset_event, identifer_event = lst
+            if word_offset == offset_event:
+                word_event = "B-3_"+identifer_event
+        # Event in [offset_begin. offset_end, "str"] format.
+        elif(isinstance(lst[2], str)):
+            begin_event, end_event, identifer_event = lst
+            if word_offset == begin_event:
+                word_event = "B-3_"+identifer_event
+            elif begin_event < word_offset <= end_event:
+                word_event = "I-3_"+identifer_event
+
+    if debug:
+        print(f"{word_offset} {word_ner} {word_relation} {word_event}")
+
+    return (word_ner, word_relation, word_event)
+
+
+def preprocess(process_type, filename, debug=False):
+    lines = []
+
+    # Set maximum line limit in case there's something
+    # wrong with file input.
+    LINE_LIMIT = 1000000
+
     print("="*20)
     print(f"Preprocessing file {filename} in {process_type} mode...")
+
     with open(filename, mode='r') as json_file:
 
-        # Set maximum line limit in case there's something
-        # wrong with file input.
-        LINE_LIMIT = 1000000
-
-        current_document = str()
+        current_document = ""
         # Store output result line by line in list.
         # e.g. lines = ["A B O O O", "C D O O O"]
-        lines = []
 
         while --LINE_LIMIT:
             # Parse json objects line by line.
@@ -24,7 +114,7 @@ def preprocess(process_type, filename, debug=False):
                 break
 
             if debug:
-                print(f"sentence: {str_jsonobj}")
+                print(f"current_line: {str_jsonobj}")
 
             jsonobj = json.loads(str_jsonobj)
             sentence = jsonobj["sentence"]
@@ -32,9 +122,11 @@ def preprocess(process_type, filename, debug=False):
             ner = jsonobj["ner"]
             relation = jsonobj["relation"]
             event = jsonobj["event"]
+            event = flatten(event)
 
             if debug:
                 print(f"sentence: {jsonobj['sentence']}")
+                print(f"current_document: {current_document}")
                 print(f"s_start: {jsonobj['s_start']}")
                 print(f"ner: {jsonobj['ner']}")
                 print(f"relation: {jsonobj['relation']}")
@@ -42,25 +134,15 @@ def preprocess(process_type, filename, debug=False):
 
             # Assign current document name
             if s_start == 0:
-                current_document = sentence
+                current_document = sentence[0]
                 continue
 
-            for i in len(sentence):
+            for i in range(len(sentence)):
                 word_offset = s_start+i
                 word = sentence[i]
-                word_ner, word_relation, word_event = \
-                    get_attributes(word_offset, ner, relation, event)
-                # TODO: Implement get_attributes.
-                # ner, ralation and event may be more than one tokens.
-                # The first token is B-${TOKEN}, following tokens are
-                # I-${TOKEN}.
-                # If list is empty, for example, len(event) = 0, then
-                # return 'O'.
-
-                if debug:
-                    print(f"word_offset: {word_offset}, word_ner: {word_ner},\
-                            word_relation: {word_relation}, word_event:\
-                          {word_event}")
+                word = replace_bracket(word)
+                word_ner, word_relation, word_event = get_attributes(
+                    word_offset, ner, relation, event, debug)
 
                 list_line = [word, current_document, word_ner, word_relation,
                              word_event]
@@ -73,14 +155,19 @@ def preprocess(process_type, filename, debug=False):
                 # Append this record to lines.
                 lines.append(str_line)
 
-        assert LINE_LIMIT == 0
+            print("-"*10)
 
-    if debug:
+        assert LINE_LIMIT >= 0
+
+    # if debug:
+    #     for str in lines:
+    #         print(str, end='')
+
+    with open(
+            'output/'+re.sub(r'^.*/', '', filename)+'.'+process_type, mode='w') \
+            as output_file:
         for str in lines:
-            print(str, end='')
-
-    with open('output/filename.'+process_type, mode='w') as output_file:
-        output_file.write(str for str in lines)
+            output_file.write(str)
 
     print("End of processing.")
     print("="*20)
@@ -97,13 +184,14 @@ if __name__ == "__main__":
         help='Type of data to be preprocessed. \
                 Available types: dev, test, train')
     parser.add_argument(
-        '--debug", help="Get more output info for debug purpose.',
+        '--debug', help='Get more output info for debug purpose.',
         default=False
     )
 
     args = parser.parse_args()
     filename = args.data
     process_type = args.type
+    debug = args.debug
 
     print(f"filename[{filename}], process_type[{process_type}]")
-    preprocess(process_type, filename)
+    preprocess(process_type, filename, debug)
