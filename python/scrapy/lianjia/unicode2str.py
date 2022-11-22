62ea70bd7ef4f76e3397c2c72a31636ae42f2a99
Micuks <sdkjwql@gmail.com>
2022-11-22 21:53:56 +0800

    [Scrapy] Linajia second hand house spider finished.

--- python/scrapy/lianjia/unicode2str.py
+++ python/scrapy/lianjia/unicode2str.py
@@ -1,16 +1,28 @@
 import json
 
-in_filename = 'new_house.json'
-out_filename = 'converted_new_house.json'
-json_formatted_str = str()
 
-with open(in_filename, 'r') as json_file:
-    json_list = json.load(json_file)
-    json_formatted_str = json.dumps(
-        json_list, indent=4, ensure_ascii=False).encode('utf-8').decode('utf-8')
-    # print(json_formatted_str.decode('utf-8'))
+def convert(in_filename, out_filename):
+    with open(in_filename, 'r') as json_file:
+        json_list = json.load(json_file)
+        json_formatted_str = json.dumps(
+            json_list, indent=4, ensure_ascii=False).encode('utf-8').decode('utf-8')
+        # print(json_formatted_str.decode('utf-8'))
 
-print(f"Converted {len(json_list)} records.")
+    print(f"Converted {len(json_list)} records from file {in_filename}.")
 
-with open(out_filename, 'w') as json_file:
-    json_file.write(json_formatted_str)
+    with open(out_filename, 'w') as json_file:
+        json_file.write(json_formatted_str)
+
+    return json_formatted_str
+
+
+if __name__ == "__main__":
+    in_files = ['new_house.json', 'second_hand.json']
+    out_files = ['converted_new_house.json', 'converted_second_hand.json']
+    json_formatted_str = str()
+
+    for i in range(len(in_files)):
+        in_filename = in_files[i]
+        out_filename = out_files[i]
+        json_formatted_str = convert(in_filename, out_filename)
+        # print(json_formatted_str)
