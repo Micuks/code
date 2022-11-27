053cf3b432f67277ef7f65b0fe45a2e779948e0f
Micuks <sdkjwql@gmail.com>
2022-11-27 21:38:55 +0800

    [Dijkstra] Cpp version.

---
+++ algorithm/Greedy/Dijkstra/src/utils/CLIParser.hpp
@@ -0,0 +1,29 @@
+#ifndef CLI_PARSER_H
+#define CLI_PARSER_H
+#include <exception>
+#include <string>
+#include <unordered_map>
+
+// class invalid_argument : public std::exception {
+//   virtual const char* what() const throw() {
+//     return "Invalid argument"
+//   }
+// }
+
+/**
+ * A simple tool to parse command line args passed to the program.
+ */
+class CLIParser {
+  private:
+    int argc;
+    char **argv;
+    void hashArgs();
+    std::string helpStr();
+
+  public:
+    std::unordered_map<std::string, std::string> args;
+
+    CLIParser(int argc, char **argv) : argc(argc), argv(argv) {}
+    void argParse();
+};
+#endif
