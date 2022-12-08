a221397ed0011221fa37271411ca1b642ee6693a
Micuks <sdkjwql@gmail.com>
2022-12-08 12:16:59 +0800

    [python] Modified run.sh to let it run all dataset by default.

--- python/pm2.5_data_process/run.sh
+++ python/pm2.5_data_process/run.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # sample process for testing.
-python src/main.py \
-    -i data/sample.csv \
-    -o data/processed_sample.csv
+# python src/main.py \
+#     -i data/sample.csv \
+#     -o data/processed_sample.csv
 
 # # process Beijing PM2.5 data.
-# python src/main.py \
-#     -i ./data/BeijingPM20100101_20151231.csv \
-#     -o ./data/processed_BeijingPM20100101_20151231.csv
+python src/main.py \
+    -i ./data/BeijingPM20100101_20151231.csv \
+    -o ./data/processed_BeijingPM20100101_20151231.csv
