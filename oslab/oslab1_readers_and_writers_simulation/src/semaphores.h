fcd3434241a11946afbe522cfd3f3cef10d3af1a
Micuks <sdkjwql@gmail.com>
2022-12-13 15:48:52 +0800

    [oslab] oslab1. shared memory.

--- oslab/oslab1_readers_and_writers_simulation/src/semaphores.h
+++ oslab/oslab1_readers_and_writers_simulation/src/semaphores.h
@@ -7,37 +7,37 @@
 
 /**
  * @brief Create and initialize semaphore.
- * 
- * @param key 
- * @param sem_num 
- * @return int 
+ *
+ * @param key
+ * @param sem_num
+ * @return int
  */
 int sem_init(key_t key, int sem_num);
 
 /**
  * @brief Delete semaphore.
- * 
- * @param sem_id 
- * @param sem_num 
- * @return int 
+ *
+ * @param sem_id
+ * @param sem_num
+ * @return int
  */
 int sem_del(int sem_id, int sem_num);
 
 /**
  * @brief Semaphore up.
- * 
- * @param sem_id 
- * @param sem_num 
- * @return int 
+ *
+ * @param sem_id
+ * @param sem_num
+ * @return int
  */
 int sem_up(int sem_id, int sem_num);
 
 /**
  * @brief Sem down.
- * 
- * @param sem_id 
- * @param sem_num 
- * @return int 
+ *
+ * @param sem_id
+ * @param sem_num
+ * @return int
  */
 int sem_down(int sem_id, int sem_num);
 
