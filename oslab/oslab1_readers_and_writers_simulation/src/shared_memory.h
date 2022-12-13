3c5fed77f44cdc042cbbf9abb6ddb632115976fc
Micuks <sdkjwql@gmail.com>
2022-12-13 17:33:31 +0800

    [oslab] oslab1: Finished reader-writer simulation main process.

--- oslab/oslab1_readers_and_writers_simulation/src/shared_memory.h
+++ oslab/oslab1_readers_and_writers_simulation/src/shared_memory.h
@@ -7,38 +7,38 @@ typedef struct shm_data {
     int reads;
     int writes;
     long double avg_time;
-} shm_data;
+} ShmData;
 
 /**
  * @brief Initialize shared memory.
- * 
- * @param key 
- * @param size 
- * @return int 
+ *
+ * @param key
+ * @param size
+ * @return int
  */
 int shm_init(key_t key, int size);
 
 /**
  * @brief Return a pointer to memory.
- * 
- * @param shm_id 
- * @return shm_data* 
+ *
+ * @param shm_id
+ * @return shm_data*
  */
-shm_data *shm_at(int shm_id);
+ShmData *shm_at(int shm_id);
 
 /**
  * @brief Detach memory
- * 
- * @param shm_ptr 
- * @return int 
+ *
+ * @param shm_ptr
+ * @return int
  */
-int shm_detach(shm_data *shm_ptr);
+int shm_detach(ShmData *shm_ptr);
 
 /**
  * @brief Deallocate memory
- * 
- * @param shm_id 
- * @return int 
+ *
+ * @param shm_id
+ * @return int
  */
 int shm_delete(int shm_id);
 
