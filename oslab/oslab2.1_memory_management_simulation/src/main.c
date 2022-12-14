b5876ee483eebde47dab0977b26853efe38cd8c0
Micuks <sdkjwql@gmail.com>
2022-12-14 17:07:45 +0800

    [oslab] oslab2.1 Finished first and best fit memory management simulation.

--- oslab/oslab2.1_memory_management_simulation/src/main.c
+++ oslab/oslab2.1_memory_management_simulation/src/main.c
@@ -1,4 +1,5 @@
 #include "schemas.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,19 +9,37 @@ int main() {
     Process *p = NULL;
     Block *b = NULL;
 
+    load_config(&p, &b, &num_processes, &num_blocks);
+
     do {
         print_menu();
-        scanf("%c", &ch);
+
+        ch = get_valid_option();
+
         switch (ch) {
         case '0':
-            config(p, b, &num_processes, &num_blocks);
+            config(&p, &b, &num_processes, &num_blocks);
+            assert(p != NULL);
+            assert(b != NULL);
             break;
+
         case '1':
             re_init(p, b, num_processes, num_blocks);
+            printf("\nFirst fit.\n");
             first_fit(p, b, num_processes, num_blocks);
             break;
+
+        case '2':
+            re_init(p, b, num_processes, num_blocks);
+            printf("\nBest fit.\n");
+            best_fit(p, b, num_processes, num_blocks);
+            break;
+
+        case 'q':
+            break;
+
         default:
-            printf("\n Invalid option.");
+            printf("\nInvalid option.");
         }
     } while (valid_option(ch));
 
