9d4dca83464deb49a6e5e8d4f97e99123c486756
Micuks <sdkjwql@gmail.com>
2022-12-24 12:17:07 +0800

    [scrapy] Beijing communitys page spider.

--- python/final/lianjia/items.py
+++ python/final/lianjia/items.py
@@ -12,7 +12,16 @@ class LianjiaItem(scrapy.Item):
     pass
 
 class BusinessAreaItem(scrapy.Item):
-    # 商圈
+    # Business area
     businessAreaUrl = scrapy.Field()
     businessAreaName = scrapy.Field()
     businessAreaRegion = scrapy.Field()
+    
+class CommunityItem(scrapy.Item):
+    # Community
+    communityUrl = scrapy.Field()
+    communityName = scrapy.Field()
+    communityRegion = scrapy.Field()
+    communityBusinessArea = scrapy.Field()
+    communityRentUrl = scrapy.Field()
+    communityAccessBit = scrapy.Field()
