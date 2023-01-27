8bc05ac52bcc5bfc033b2a089ffc8de2b62a1a6e
Micuks <sdkjwql@gmail.com>
2023-01-27 20:53:11 +0800

    GRU Cell

--- nlp/MLBiNetPytorch/layers.py
+++ nlp/MLBiNetPytorch/layers.py
@@ -82,6 +82,14 @@ class PeepHoleLSTMCELL(nn.Module):
         self.bias_hh = Parameter(torch.randn(4 * hidden_size))
         self.bias_ch = Parameter(torch.randn(4 * hidden_size))
 
+    def reset_parameters(self) -> None:
+        """
+        The same initialization method as LSTM in nn.Module.
+        """
+        std = 1.0 / self.hidden_size
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
     def forward(
         self, input: Tensor, state: Tuple[Tensor, Tensor]
     ) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
@@ -89,8 +97,10 @@ class PeepHoleLSTMCELL(nn.Module):
         gates = (
             (torch.mm(input, self.weight_ih.t()))
             + self.bias_ih
-            + torch.mm(hx, self.weight_hh.t() + self.bias_hh)
-            + torch.mm(cx, self.weight_ch.t() + self.bias_ch)
+            + torch.mm(hx, self.weight_hh.t())
+            + self.bias_hh
+            + torch.mm(cx, self.weight_ch.t())
+            + self.bias_ch
         )
         # TODO: computation of cx above may be wrong during (maybe) wrong shape.
 
@@ -105,6 +115,54 @@ class PeepHoleLSTMCELL(nn.Module):
         hy = outgate * torch.tanh(cy)
 
         return hy, (hy, cy)
+
+
+class GRUCell(nn.Module):
+    """
+    GRU(Gated Recurrent Unit) LSTM Cell
+
+    Combines the forget and input gate into update gate which is newly added in
+    this architecture. It also merges the cell state and hidden state. The
+    resulting model is simpler than traditional LSTM.
+    """
+
+    def __init__(self, input_size, hidden_size, bias=True) -> None:
+        super(GRUCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.weight_ih = Parameter(torch.randn(3 * hidden_size, input_size))
+        self.weight_rh = Parameter(torch.randn(3 * hidden_size, hidden_size))
+        self.bias_ih = Parameter(torch.randn(3 * hidden_size))
+        self.bias_rh = Parameter(torch.randn(3 * hidden_size))
+
+    def reset_parameters(self) -> None:
+        """
+        The same initialization method as LSTM in nn.Module.
+        """
+        std = 1.0 / self.hidden_size
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, input: Tensor, state: Tensor) -> Tensor:
+        hx = state
+        gates_x = torch.mm(input, self.weight_ih.t()) + (
+            self.bias_ih if self.bias else 0
+        )
+        gates_h = +torch.mm(hx, self.weight_rh.t()) + (
+            self.bias_rh if self.bias else 0
+        )
+
+        ingate_x, resetgate_x, newgate_x = gates_x.chunk(3, 1)
+        ingate_h, resetgate_h, newgate_h = gates_h.chunk(3, 1)
+
+        ingate = torch.sigmoid(ingate_x + ingate_h)
+        resetgate = torch.sigmoid(resetgate_x + resetgate_h)
+        newgate = torch.tanh(newgate_h * resetgate + newgate_x)
+
+        hy = (1 - ingate) * hx + resetgate * newgate
+        
+        return hy
 
 
 class SentenceEncodingLayer(nn.Module):
