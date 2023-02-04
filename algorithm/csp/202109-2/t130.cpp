14706f7b140ce3c882a180ebee6c5a74619f7918
Micuks <sdkjwql@gmail.com>
2023-02-04 15:55:50 +0800

    t130 wa 60pts

--- algorithm/csp/202109-2/t130.cpp
+++ algorithm/csp/202109-2/t130.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <climits>
 #include <cstring>
 #include <iostream>
@@ -10,37 +11,47 @@ class Solution {
     vector<int> a;
     int aMax;
     int aMin;
-    int solve();
+    static const int MAXN = 2e4;
+    int count[MAXN];
+    int ans;
 
   public:
     Solution(int n, vector<int> a, int aMax, int aMin)
         : n(n), a(a), aMax(aMax), aMin(aMin) {
-        ans = solve();
+        memset(count, 0, (aMax - aMin + 1) * sizeof(int));
     }
-    int ans;
+    int solve();
 };
 
 int Solution::solve() {
-    ans = INT_MIN;
-    for (int p = aMin; p <= aMax; p++) {
-        int cnt = 0;
-        int flag = 0;
-        for (int i = 0; i < a.size(); i++) {
-            if (flag == 1) {
-                if (a[i] > p) {
-                    flag = 1;
-                } else {
-                    flag = 0;
-                }
-            } else if (flag == 0) {
-                if (a[i] > p) {
-                    flag = 1;
-                    cnt++;
-                }
-            }
-        }
-        ans = cnt > ans ? cnt : ans;
+    n = unique(a.begin(), a.end()) - a.begin();
+
+    if (a[0] > a[1]) {
+        count[a[0]]++;
+    } else if (a[0] < a[1]) {
+        count[a[0]]--;
     }
+    if (a[n - 1] > a[n - 2]) {
+        count[a[n - 1]]++;
+    } else if (a[n - 1] < a[n - 2]) {
+        count[a[n - 1]]--;
+    }
+
+    for (int i = 1; i < n - 1; i++) {
+        if (a[i - 1] < a[i] && a[i] > a[i + 1]) {
+            count[a[i]]++;
+        } else if (a[i - 1] > a[i] && a[i + 1] > a[i]) {
+            count[a[i]]--;
+        }
+    }
+
+    ans = 0;
+    int tmp = 0;
+    for (int num = aMax; num >= aMin; num--) {
+        tmp = tmp + count[num];
+        ans = ans > tmp ? ans : tmp;
+    }
+
     return ans;
 }
 
@@ -61,7 +72,7 @@ int main() {
     }
 
     Solution s(n, a, aMax, aMin);
-    cout << s.ans << endl;
+    cout << s.solve() << endl;
 
     return 0;
 }
