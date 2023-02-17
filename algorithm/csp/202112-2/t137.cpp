e25442ce29e1078cebe407b7be657b1d83091f32
Micuks <sdkjwql@gmail.com>
2023-02-18 01:16:41 +0800

    int64_t 100pts to csp 202112-2

--- algorithm/csp/202112-2/t137.cpp
+++ algorithm/csp/202112-2/t137.cpp
@@ -1,8 +1,11 @@
+#include <climits>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 using namespace std;
 
+#define int int64_t
 class Solution {
     int n, N;
     vector<int> A;
@@ -20,35 +23,38 @@ int Solution::solve() {
     err = 0;
     int i = 0, j = 0;
     int f = 0, g = 0;
+    int dr = r;
+    int ddr = 0;
+    int flag = 0;
     // i iterates through A, j iterates through 0...N-1
-    while (i < n && j < N) {
-        if (j >= A[i] && j < A[i + 1]) {
-            f = i;
-            g = j / r;
-            err += abs(f - g);
-            j++;
-        } else if (j < A[i]) {
-            g = j / r;
-            err += abs(f - g);
-            j++;
-        } else if (j >= A[i + 1]) {
-            i++;
-        }
-    }
+    for (i = 0; i <= n; i++) {
+        f = i;
 
-    if (i >= n) {
-        f = n;
-        while (j < N) {
+        for (j = A[i]; j < A[i + 1]; j += dr) {
+            if (flag) {
+                dr = ddr;
+                flag = 0;
+            } else {
+                dr = r;
+            }
+
             g = j / r;
-            err += abs(f - g);
-            j++;
+            if (j + dr <= A[i + 1]) {
+                err += abs(f - g) * dr;
+            } else {
+                err += abs(f - g) * (A[i + 1] - j);
+                ddr = dr - (A[i + 1] - j);
+                flag = 1;
+            }
         }
     }
 
     return err;
 }
 
+#undef int
 int main() {
+#define int int64_t
     int n, N;
     cin >> n >> N;
     vector<int> A;
@@ -58,6 +64,7 @@ int main() {
         cin >> tmp;
         A.push_back(tmp);
     }
+    A.push_back(N);
 
     Solution s(n, N, A);
     cout << s.solve() << endl;
