042abffdb5a2e1afee0018620842a03f4da0c3ae
Micuks <sdkjwql@gmail.com>
2023-02-21 22:30:09 +0800

    regex pattern matching yyyy-MM-dd hh:mm:ss Name etc.

--- script.py
+++ script.py
@@ -1,15 +1,12 @@
-def sum(a,b):
-    a=a+b
-    return a
+import re
 
-def ex(a,b):
-    c=a
-    a=b
-    b=c
-
-a=1
-b=2
-sum(a,b)
-ex(a,b)
-print(a,b)
-
+strs =[ "2022-10-21 15:11:35 吴清柳<imwql@qq.com>\n",
+       "2022-10-21 15:11:44 Mr.Goddess(215381250)\n", "没用的登西\n"]
+# pattern = r"^\d+-\d+-\d+\ \d+:\d+:\d+\ .*$"
+pattern = r"^\d+-\d+-\d+\s\d+:\d+:\d+\s.*$"
+match = [re.search(pattern, str) for str in strs]
+for m, s in zip(match, strs):
+    if(isinstance(m, re.Match)):
+        print(m.group(0))
+    else:
+        print("No match in {}".format(s))
