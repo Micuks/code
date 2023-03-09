b734a58db7b1e47de5f1205f198f3e54c1a5a497
Micuks <sdkjwql@gmail.com>
2023-03-09 09:21:16 +0800

    Hadoop operation in Java

---
+++ javaHadoop/java-hadoop/src/main/java/com/wql-2020211597/app/App.java
@@ -0,0 +1,11 @@
+// package com.wql-2020211597.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
