cddb479a6204fc14a211ac0ddc7d2e4164ff528c
Micuks <sdkjwql@gmail.com>
2023-04-08 14:49:03 +0800

    Hadoop lab3 HBase MemberMapper class

---
+++ javaHadoop/lab3.hbase.inputSource/src/main/java/org/wql2020211597/hbase/inputSource/App.java
@@ -0,0 +1,13 @@
+package org.wql2020211597.hbase.inputSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ javaHadoop/lab3.hbase.inputSource/src/test/java/org/wql2020211597/hbase/inputSource/AppTest.java
@@ -0,0 +1,20 @@
+package org.wql2020211597.hbase.inputSource;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
