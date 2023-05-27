424bce33607d6b02d7736f14d1ef1066ac90a8df
Micuks <sdkjwql@gmail.com>
2023-05-27 11:03:35 +0800

    modifies

--- javaHadoop/final.lab1/src/main/java/com/wql_2020211597/app/FlinkKafka.java
+++ javaHadoop/final.lab1/src/main/java/com/wql_2020211597/app/FlinkKafka.java
@@ -6,11 +6,9 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.KafkaSourceOptions;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 
 /**
