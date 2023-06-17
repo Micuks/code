0f56a7d9840a91daf284266966f125bf87fa29bf
Micuks <sdkjwql@gmail.com>
2023-06-17 21:48:32 +0800

    bigdata final lab2 fix bugs

--- BigDataLabs/final_lab2/my_create_table.sql
+++ BigDataLabs/final_lab2/my_create_table.sql
@@ -1,14 +1,18 @@
-DROP TABLE IF EXISTS `dli-wql`.`daily_book_sales`;
+DROP TABLE IF EXISTS `dli-wql`.`daily_book_sales_stats`;
 
-CREATE TABLE `dli-wql`.`daily_book_sales` (
-    `date` DATE NOT NULL,
-    `book_category` VARCHAR(64) NOT NULL,
-    `total_sales` INT UNSIGNED NULL,
+CREATE TABLE `dli-wql`.`daily_book_sales_stats` (
+    `cur_date` VARCHAR(32) NOT NULL,
+    `book_category` VARCHAR(32) NOT NULL,
+    `num_titles` BIGINT UNSIGNED NULL,
+    `total_sales` BIGINT UNSIGNED NULL,
+    `avg_sales` DOUBLE UNSIGNED NULL,
+    `max_sales` BIGINT UNSIGNED NULL,
+    `min_sales` BIGINT UNSIGNED NULL,
     `total_revenue` DOUBLE UNSIGNED NULL,
-    `last_sale_time` DATETIME NULL,
-    PRIMARY KEY (`date`, `book_category`)
+    `last_sale_time` VARCHAR(32) NULL,
+    `flink_current_time` VARCHAR(32) NULL,
+    PRIMARY KEY (`cur_date`, `book_category`)
 ) ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4
   COLLATE = utf8mb4_general_ci
   COMMENT = '每日各种图书的销售统计';
-
